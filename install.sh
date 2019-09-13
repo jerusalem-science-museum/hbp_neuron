fb95915c5ac5a4398bef8843d6b1b22d02ba1e7d
arduino12 <arduino12@users.noreply.github.com>
2019-09-13 21:53:01 +0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -4,6 +4,18 @@ sudo apt install -y git git-lfs python3-pip python3-tk vlc
 pip3 install --upgrade pip
 sudo -H pip3 install --upgrade ipython Pillow pygame screeninfo python-vlc
 
+DESKTOP_PATH=/home/mada/Desktop # $HOME/Desktop
+cat > $DESKTOP_PATH/Neuron.desktop <<EOF
+[Desktop Entry]
+Name=Neuron
+Comment=Neuron
+Exec=$DESKTOP_PATH/hbp_neuron/run.sh
+Version=1.0
+Terminal=false
+Type=Application
+GenericName[en_IL]=Neuron
+EOF
+
 # sudo apt install -y python3-pil python3-pil.imagetk
 # cd Desktop
 # git clone https://github.com/jerusalem-science-museum/hbp_neuron.git
