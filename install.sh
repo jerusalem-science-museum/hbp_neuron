83674f9f5f45daa611e652317c127ac9050a41f6
arduino12 <arduino12@users.noreply.github.com>
2019-09-13 20:54:51 +0300

    Add install.sh script

---
+++ install.sh
@@ -0,0 +1,12 @@
+sudo apt update
+sudo apt upgrade -y
+sudo apt install -y git git-lfs python3-pip python3-tk vlc
+pip3 install --upgrade pip
+sudo -H pip3 install --upgrade ipython Pillow pygame screeninfo python-vlc
+
+# sudo apt install -y python3-pil python3-pil.imagetk
+# cd Desktop
+# git clone https://github.com/jerusalem-science-museum/hbp_neuron.git
+# cd hbp_neuron
+# git lfs install
+# git lfs pull
